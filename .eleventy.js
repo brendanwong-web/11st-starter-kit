ca6ae3a933d393545fe77d9a382a54b4dafee7c0
brendanwong-web <69905640+brendanwong-web@users.noreply.github.com>
2021-08-20 14:12:34 +0800

    update 2

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,35 @@ module.exports = function (config) {
     dynamicPartials: true,
   });
 
+  // shortcodes
+  config.addPairedShortcode("collapseAbout", function(content, title, color, idx) {
+    return `
+      <li class="relative" x-data="{selected:null}">
+
+      <button type="button" class="w-full px-4 py-6 text-left ${color}" @click="selected !== ${idx} ? selected = ${idx} : selected = null">
+        <div class="flex items-center justify-between text-yellow-50">
+          <span>
+            ${title}					
+                    </span>
+          <i class="fas fa-plus"></i>
+        </div>
+      </button>
+
+      <div
+            class="relative overflow-hidden transition-all max-h-0 duration-500"
+            style="" x-ref="container${idx}" 
+            x-bind:style="selected == ${idx} ? 'max-height: ' + $refs.container${idx}.scrollHeight + 'px' : ''"
+            >
+        <div class="p-4">
+          ${content}
+        </div>
+      </div>
+
+    </li>
+    `;
+  });
+
+
   // Static assets to pass through
   config.addPassthroughCopy('./src/fonts');
   config.addPassthroughCopy('./src/images');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: ['./src/**/*.html', './src/**/*.md'],
@@ -8,6 +9,33 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.skyblue,
+      purple: colors.violet,
+      green: colors.teal,
+      lhtblue: {
+        light: '#7FB6CB',
+        DEFAULT: '#84CEEB',
+        dark: '#316B82',
+        darker: '#385B61'
+      }
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'md': '1rem',
+      'md2': '1.125rem',
+      'base': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    }
     },
   },
   variants: {},
